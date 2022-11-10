5bbb326f9d9c24affe1a0cfde41dbc34e75a9ed3
munzir arzykulov <munzirrrarzykulov01@gmail.com>
2022-11-10 18:15:15 +0600

    !

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+     private  String string ;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
